fcc28955a9c18865cb83b40d7add84a15918014b
Alice Brunel <alc.brunel@gmail.com>
2019-10-03 15:56:41 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,7 +22,7 @@ use Rack::Flash
     @song.artist = Artist.find_or_create_by(:name => params["Artist Name"])
     @song.genre_ids = params[:genres]
     @song.save
-    
+
     flash[:message] = "Successfully created song."
     redirect("/songs/#{@song.slug}")
   end
